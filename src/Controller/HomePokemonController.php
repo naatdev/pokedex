71fa439857833b4f0e8c04541b4dab5df0b15c8b
Florian Hourdin <florian.hourdin59300@gmail.com>
2022-05-11 17:23:35 +0200

    Start pokemon details page

--- src/Controller/HomePokemonController.php
+++ src/Controller/HomePokemonController.php
@@ -13,11 +13,24 @@ class HomePokemonController extends AbstractController
     public function index(PokeApiService $pokeApiService): Response
     {
         // get pokemon species for a specific generation
-        $pokemon_species = $pokeApiService->getPokemonsByGeneration($_ENV['POKE_GENERATION'])['pokemon_species'];
+        $pokemon_species = $pokeApiService->getPokemonsByGeneration($_ENV['POKE_GENERATION']);
 
         // render view with pokemon species and geenration number
         return $this->render('home_pokemon/index.html.twig', [
             'generation_number' => $_ENV['POKE_GENERATION'],
+            'pokemon_species'   => $pokemon_species,
+        ]);
+    }
+
+    #[Route('/pokemon/{id}', name: 'pokemon_details')]
+    public function details($id, PokeApiService $pokeApiService): Response
+    {
+        // get pokemon species for a specific generation
+        $pokemon_species = $pokeApiService->getPokemonsByGeneration($_ENV['POKE_GENERATION']);
+
+        // render view with pokemon species and geenration number
+        return $this->render('home_pokemon/pokemon_details.html.twig', [
+            'pokemon_id'        => $id,
             'pokemon_species'   => $pokemon_species,
         ]);
     }
--- src/Service/PokeApiService.php
+++ src/Service/PokeApiService.php
@@ -20,6 +20,28 @@ class PokeApiService
             'https://pokeapi.co/api/v2/generation/' . $generation
         );
 
-        return $response->toArray();
+        $response = $response->toArray()['pokemon_species'];
+        $pokemons = [];
+
+        foreach($response as $pokemon) {
+            // retrieve pokemon id from URL
+            $explode_url = explode('/', $pokemon['url']);
+            $count = count($explode_url);
+            $id = 0;
+
+            // in case of bad http response check if we have an array
+            $pos = $count - 2;
+            if(isset($explode_url[$pos])) {
+                $id = $explode_url[$pos];
+            }
+
+            // create new pokemon array item
+            $pokemons[] = [
+                'id'    => $id,
+                'name'  => $pokemon['name'],
+            ];
+        }
+
+        return $pokemons;
     }
 }
